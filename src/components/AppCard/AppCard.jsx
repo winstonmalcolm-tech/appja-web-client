8cbb3044a454d7d503437357f22d0ff58ed6e334
Winston Malcolm <winstionmalcolm77@gmail.com>
2024-11-17 10:43:37 -0500

    profile edited

---
+++ src/components/AppCard/AppCard.jsx
@@ -0,0 +1,9 @@
+import "./appCard.css";
+
+const AppCard = ({app}) => {
+  return (
+    <div>AppCard</div>
+  )
+}
+
+export default AppCard
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -3,6 +3,7 @@ import { toast } from "react-toastify";
 import { useNavigate } from 'react-router-dom';
 import "./profile.css";
 import useAxios from '../../utils/useAxios';
+import AppCard from '../../components/AppCard/AppCard';
 
 
 const INITIAL_STATE = {
@@ -72,17 +73,14 @@ const Profile = () => {
     
       } catch (error) {
         console.log(error.message);
-        toast.error("This is the catch block");
+        toast.error(error.message);
         
       }
     }
 
 
     useEffect(() => {
-
       fetchUserData(api);
-    
-        
     }, []);
 
     if (state.loading) return <h1>Loading</h1>;
@@ -91,17 +89,33 @@ const Profile = () => {
     <div className='profile_container'>
 
         <div className="profile_header">
-          <div className="profile_avatar_container">
-            {state.user.profile_image ? <img src={state.user?.profile_image } /> : <h3>{state.user.first_name.charAt(0)}{state.user?.last_name?.charAt(0)}</h3>}
-            <label htmlFor="file-upload">+</label>
-            <input type="file" id="file-upload"/>
+          <div className="profile_header_left">
+            <div className="profile_avatar_container">
+              {state.user.profile_image ? <img src={state.user?.profile_image } /> : <h3>{state.user.first_name.charAt(0)}{state.user?.last_name?.charAt(0)}</h3>}
+              <label htmlFor="file-upload">+</label>
+              <input type="file" id="file-upload"/>
+            </div>
+            
+            <div className="profile_subtitle">
+              <h1>{state.user.first_name} {state.user.last_name}</h1>
+              <h3>@{state.user.username}</h3>
+              <button>Contact</button>
+            </div>
+          </div>
+
+          <div className="profile_header_right">
+            <button>Post App</button>
+          </div>
+        </div>
+
+        <div className="profile_app_display">
+          <div className="profile_app_display_header">
+            <h1>Created Apps</h1>
           </div>
           
-          <div className="profile_subtitle">
-            <h1>{state.user.first_name} {state.user.last_name}</h1>
-            <h3>@{state.user.username}</h3>
-            <button>Contact</button>
-          </div>
+          {state.apps.length == 0 ? <h1>No Apps</h1> : null}
+
+          {state.apps.map((app) => <AppCard app={app}/>)}
         </div>
         
     </div>
